54acc490bbe31ebccabe89257a5e2d3aa717c5a6
Rodrigo Dornelles <31113760+rfdornelles@users.noreply.github.com>
2024-02-16 18:00:43 -0300

    funções auxiliares para login / identificação

---
+++ R/login.R
@@ -0,0 +1,94 @@
+
+## função para checar se a identificação foi feita
+checar_identificacao_valida <- function() {
+
+  # puxa do environment o email e a key
+  email_identificado <- Sys.getenv("datajud_email_user")
+
+  # Padrão de expressão regular para validar email
+  email_pattern <- "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+
+  # checa se os valores estão definidos
+  if (!grepl(email_pattern, email_identificado)) {
+    return(FALSE)
+
+  } else {
+    return(email_identificado)
+  }
+
+}
+
+## função para setar usuario
+
+setar_email_identificacao <- function(email = NULL) {
+
+  if(is.null(email)) {
+
+    email <- rstudioapi::showPrompt(
+      title = "Identificação",
+      message = "Por favor, forneça um email para identificação junto ao CNJ:",
+      default = ""
+    )
+
+
+  }
+  # Padrão de expressão regular para validar email
+  email_pattern <- "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+
+  # Verificar se é um email válido
+  if(grepl(email_pattern, email)) {
+    # Se for, setar environment
+    Sys.setenv(datajud_email_user = email)
+    cat("Email configurado com sucesso:", email, "\n")
+  } else {
+    cat("Email inválido. Por favor, forneça um email válido.\n")
+  }
+
+}
+
+## função auxiliar para gerar user_agent
+
+user <- function() {
+
+  email_user <- checar_identificacao_valida()
+
+  if(email_user == FALSE) {
+    return(email_user)
+  }
+
+  return(
+    httr::user_agent(paste("Pacote {datajud} para R - email:", email_user))
+  )
+
+}
+
+## função para obter key do site do CNJ
+
+obter_key_cnj <- function() {
+
+  # checar se usuario foi identificado
+  email_user <- checar_identificacao_valida()
+
+  if(email_user == FALSE) {
+
+    warning("Usuário não identificado.\nPor favor, identifique-se com um email válido usando setar_email_identificacao(seu_email).")
+    return(FALSE)
+  }
+
+  # url do site do CNJ
+  url <- "https://datajud-wiki.cnj.jus.br/api-publica/acesso/"
+
+  # puxa a key do site
+  key <- httr::GET(url,
+                   user()) |>
+    httr::content() |>
+    xml2::xml_find_all("//strong") |>
+    xml2::xml_text() |>
+    purrr::pluck(3)
+
+  # seta a key no environment
+  Sys.setenv(datajud_key = key)
+
+  cat("Key configurada com sucesso:", key, "\n")
+  invisible(key)
+}
