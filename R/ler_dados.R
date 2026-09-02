406982e21a2dd16cf098120bf3eaecdbd7aabc44
Rodrigo Dornelles <rodornelles@gmail.com>
2026-09-02 01:36:36 -0300

    [F1-I03] Estabiliza parsers de processos, assuntos e movimentos (#31)
    * F1-I03: estabiliza parsers de processos e movimentos
    
    * Revisa schemas opcionais dos parsers
    
    * Corrige verificações do R CMD check nos parsers

--- R/ler_dados.R
+++ R/ler_dados.R
@@ -1,22 +1,29 @@
 ## Funções auxiliares e de alto nível para leitura dos dados já baixados do Datajud
 
+esquema_movimentos_vazio <- function() {
+  tibble::tibble(
+    tribunal = character(), numero_processo = character(),
+    datahora_movimento = as.POSIXct(character(), tz = "UTC"),
+    codigo_tpu = integer(), nome_movimento = character(),
+    codigo_tabelado = integer(), descricao_tabelado = character(),
+    valor_tabelado = character(), nome_tabelado = character(),
+    codigo_orgao_julgador = integer(), nome_orgao_julgador = character()
+  )
+}
+
 ler_movimentos <- function(item) {
 
-  item <- purrr::pluck(item,
-                            # "hits",
-                            # "hits",
-                            # 1,
-                            "_source")
+  item <- purrr::pluck(item, "_source", .default = list())
 
-  tribunal <- purrr::pluck(item, "tribunal")
-  numero_processo <- purrr::pluck(item, "numeroProcesso")
+  tribunal <- purrr::pluck(item, "tribunal", .default = NA_character_)
+  numero_processo <- purrr::pluck(item, "numeroProcesso", .default = NA_character_)
   # data_ajuizamento <- purrr::pluck(item, "dataAjuizamento")
   # data_atualizacao <- purrr::pluck(item, "dataHoraUltimaAtualizacao")
 
-  movimento <- purrr::pluck(item, "movimentos")
+  movimento <- purrr::pluck(item, "movimentos", .default = list())
 
-  if (is.null(movimento)) {
-    return(NULL)
+  if (length(movimento) == 0L) {
+    return(esquema_movimentos_vazio())
   }
 
   #print(movimento)
@@ -28,22 +35,16 @@ ler_movimentos <- function(item) {
     .f = ~{
 
   tibble::tibble(
-    codigo_tpu = purrr::pluck(.x, "codigo"),
-    nome_movimento = purrr::pluck(.x, "nome"),
-    datahora_movimento = purrr::pluck(.x, "dataHora"),
-    codigo_tabelado = purrr::pluck(.x, "complementosTabelados", 1, "codigo"),
-    descricao_tabelado = purrr::pluck(.x, "complementosTabelados", 1, "descricao"),
-    valor_tabelado = purrr::pluck(.x, "complementosTabelados", 1, "valor"),
-    nome_tabelado = purrr::pluck(.x, "complementosTabelados", 1, "nome"),
-    codigo_orgao_julgador = purrr::pluck(.x, "orgaoJulgador", 1, "codigoOrgao"),
-    nome_orgao_julgador = purrr::pluck(.x, "orgaoJulgador", 1, "nomeOrgao")
-  ) |>
-    dplyr::mutate(
-      dplyr::across(
-        dplyr::everything(),
-        ~ifelse(is.null(.x), NA, .x)
-      )
-    )
+    codigo_tpu = purrr::pluck(.x, "codigo", .default = NA_integer_),
+    nome_movimento = purrr::pluck(.x, "nome", .default = NA_character_),
+    datahora_movimento = purrr::pluck(.x, "dataHora", .default = NA_character_),
+    codigo_tabelado = purrr::pluck(.x, "complementosTabelados", 1, "codigo", .default = NA_integer_),
+    descricao_tabelado = purrr::pluck(.x, "complementosTabelados", 1, "descricao", .default = NA_character_),
+    valor_tabelado = purrr::pluck(.x, "complementosTabelados", 1, "valor", .default = NA_character_),
+    nome_tabelado = purrr::pluck(.x, "complementosTabelados", 1, "nome", .default = NA_character_),
+    codigo_orgao_julgador = purrr::pluck(.x, "orgaoJulgador", 1, "codigoOrgao", .default = NA_integer_),
+    nome_orgao_julgador = purrr::pluck(.x, "orgaoJulgador", 1, "nomeOrgao", .default = NA_character_)
+  )
   })
 
   tabela_movimentos <- tabela_movimentos |>
@@ -67,36 +68,35 @@ ler_movimentos <- function(item) {
 ler_processo <- function(dados) {
 
   # Extrair o item do objeto de dados
-  item <- purrr::pluck(dados,
-                       "_source")
+  item <- purrr::pluck(dados, "_source", .default = list())
 
-  # Extrair o ID do processo
-  id <- purrr::pluck(item, "id")
+  id <- purrr::pluck(item, "id", .default = NA_character_)
 
-  # Verificar se o ID é nulo e retornar NULL se for
-  if(is.null(id)) {
-    return(NULL)
+  if (length(id) != 1L || is.na(id) || !nzchar(id)) {
+    rlang::abort("O campo id \u00e9 obrigat\u00f3rio para identificar o processo.")
   }
 
   ## Extrair os dados do processo
-  tribunal <- purrr::pluck(item, "tribunal")
-  numero_processo <- purrr::pluck(item, "numeroProcesso")
-  data_ajuizamento <- purrr::pluck(item, "dataAjuizamento")
-  data_atualizacao <- purrr::pluck(item, "dataHoraUltimaAtualizacao")
-  grau <- purrr::pluck(item, "grau")
-  nivel_sigilo <- purrr::pluck(item, "nivelSigilo")
-  formato <- purrr::pluck(item, "formato", "nome")
-  sistema <- purrr::pluck(item, "sistema", "nome")
-  classe_tpu <- purrr::pluck(item, "classe", "codigo")
-  classe_nome <- purrr::pluck(item, "classe", "nome")
-  assuntos <- purrr::pluck(item, "assuntos") |>
-    purrr::map_chr(
-      ~paste0( .x$codigo, " / ", .x$nome,
-               collapse = " | ")
-    )
-  orgao_julgador_codigo <- purrr::pluck(item, "orgaoJulgador", "codigo")
-  orgao_julgador_nome <- purrr::pluck(item, "orgaoJulgador", "nome")
-  orgao_julgador_ibge <- purrr::pluck(item, "orgaoJulgador", "codigoMunicipioIBGE")
+  tribunal <- purrr::pluck(item, "tribunal", .default = NA_character_)
+  numero_processo <- purrr::pluck(item, "numeroProcesso", .default = NA_character_)
+  data_ajuizamento <- purrr::pluck(item, "dataAjuizamento", .default = NA_character_)
+  data_atualizacao <- purrr::pluck(item, "dataHoraUltimaAtualizacao", .default = NA_character_)
+  grau <- purrr::pluck(item, "grau", .default = NA_character_)
+  nivel_sigilo <- purrr::pluck(item, "nivelSigilo", .default = NA_integer_)
+  formato <- purrr::pluck(item, "formato", "nome", .default = NA_character_)
+  sistema <- purrr::pluck(item, "sistema", "nome", .default = NA_character_)
+  classe_tpu <- purrr::pluck(item, "classe", "codigo", .default = NA_integer_)
+  classe_nome <- purrr::pluck(item, "classe", "nome", .default = NA_character_)
+  assuntos <- purrr::pluck(item, "assuntos", .default = list())
+  assuntos_tbl <- purrr::map_dfr(assuntos, ~tibble::tibble(
+    codigo = purrr::pluck(.x, "codigo", .default = NA_integer_),
+    nome = purrr::pluck(.x, "nome", .default = NA_character_)
+  ))
+  assuntos_resumo <- if (nrow(assuntos_tbl) == 0L) NA_character_ else
+    paste0(assuntos_tbl$codigo, " / ", assuntos_tbl$nome, collapse = " | ")
+  orgao_julgador_codigo <- purrr::pluck(item, "orgaoJulgador", "codigo", .default = NA_integer_)
+  orgao_julgador_nome <- purrr::pluck(item, "orgaoJulgador", "nome", .default = NA_character_)
+  orgao_julgador_ibge <- purrr::pluck(item, "orgaoJulgador", "codigoMunicipioIBGE", .default = NA_integer_)
 
   # Criar um tibble com os dados do processo
   processo <- tibble::tibble(
@@ -111,17 +111,12 @@ ler_processo <- function(dados) {
     sistema = sistema,
     classe_tpu = classe_tpu,
     classe_nome = classe_nome,
-    assuntos = assuntos,
+    assuntos = list(assuntos_tbl),
+    assuntos_resumo = assuntos_resumo,
     orgao_julgador_codigo = orgao_julgador_codigo,
     orgao_julgador_nome = orgao_julgador_nome,
     orgao_julgador_ibge = orgao_julgador_ibge
-  ) |>
-    dplyr::mutate(
-      dplyr::across(
-        dplyr::everything(),
-        ~ifelse(is.null(.x), NA, .x)
-      )
-    )
+  )
 
   # Converter as colunas de data para formato datetime
   processo <- processo |>
@@ -133,18 +128,35 @@ ler_processo <- function(dados) {
   return(processo)
 }
 
+#' Desaninha os assuntos estruturados dos processos
+#'
+#' @param dados Resultado de `datajud_ler_processo()`.
+#' @return Tibble com uma linha por processo e assunto.
+#' @export
+datajud_desaninhar_assuntos <- function(dados) {
+  if (!is.data.frame(dados) || !"assuntos" %in% names(dados)) {
+    stop("dados deve conter a coluna assuntos")
+  }
+  purrr::map_dfr(seq_len(nrow(dados)), function(i) {
+    assuntos <- dados$assuntos[[i]]
+    if (!is.data.frame(assuntos) || nrow(assuntos) == 0L) {
+      return(tibble::tibble(
+        id = character(), codigo = integer(), nome = character()
+      ))
+    }
+    dplyr::mutate(assuntos, id = dados$id[[i]], .before = 1L)
+  })
+}
+
 ### Função para ler os dados de um processo
 
 #' Lê os dados de processos retornados pelo Datajud
 #'
 #' Após realizar uma pesquisa de processos com `datajud_pesquisar_classe_orgao`,
 #' esta função permite ler e manipular os dados dos processos retornados.
-#' Aceita tanto uma lista diretamente quanto o nome de uma variável global que contém a lista de processos.
-#'
-#' @param base Lista de processos ou o nome de uma variável global que contém os dados dos processos.
 #' @param base Lista de respostas de processos retornadas pela API.
 #'
-#' @return Imprime e retorna um data frame contendo os metadados dos processos.
+#' @return Um tibble contendo os metadados dos processos.
 #'
 #' @export
 #'
